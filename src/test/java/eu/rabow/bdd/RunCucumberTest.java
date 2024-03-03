71734c813e7ff67e00c6515f7befa57227e0ba2c
Katrin.Rabow <git@rabow.eu>
2024-03-03 10:53:53 +0100

    Initiales Setup

---
+++ src/test/java/eu/rabow/bdd/RunCucumberTest.java
@@ -0,0 +1,13 @@
+package eu.rabow.bdd;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        snippets = CucumberOptions.SnippetType.CAMELCASE)
+
+public class RunCucumberTest {
+
+}
