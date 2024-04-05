362bd6fae64b30e29dac8ab73fa3dd8ec659aa68
Katrin.Rabow <git@rabow.eu>
2024-04-05 21:51:41 +0200

    Erstelle leere StepDefinitions

---
+++ src/test/java/eu/rabow/bdd/StepDefinitions.java
@@ -0,0 +1,62 @@
+package eu.rabow.bdd;
+
+import io.cucumber.java.de.*;
+
+public class StepDefinitions {
+
+    @Angenommen("alle Tische sind leer")
+    public void alleTischeSindLeer() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    Reservierung reservierung;
+    @Wenn("eine Person reserviert einen Platz")
+    public void einePersonReserviertEinenPlatz() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Dann("die Person bekommt einen Platz an Tisch {int}")
+    public void diePersonBekommtEinenPlatzAnTisch(Integer nummer) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Angenommen("ein Platz an einem bereits teilweise besetzten Tisch ist frei")
+    public void einPlatzAnEinemBereitsTeilweiseBesetztenTischIstFrei() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Dann("die Person bekommt einen Platz an einem bereits teilweise besetzten Tisch")
+    public void diePersonBekommtEinenPlatzAnEinemBereitsTeilweiseBesetztenTisch() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Gegebensei("kein Platz an einem bereits besetzten Tisch ist frei")
+    public void keinPlatzAnEinemBereitsBesetztenTischIstFrei() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Gegebensei("ein Tisch ist leer")
+    public void einTischIstLeer() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Dann("die Person bekommt einen Platz an dem neuen Tisch")
+    public void diePersonBekommtEinenPlatzAnDemNeuenTisch() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Dann("eine Person von einem anderen Tisch wird an den neuen Tisch umgesetzt")
+    public void einePersonVonEinemAnderenTischWirdAnDenNeuenTischUmgesetzt() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+}
